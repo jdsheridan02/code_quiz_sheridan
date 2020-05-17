381f62a20c0781e4f9b658e5362384fd4c951e7a
jdsheridan02 <jdsheridan02@gmail.com>
2020-05-16 22:02:10 -0400

    updated files with code for the quiz

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # code_quiz_sheridan
 Code Quiz game. Using the functionality of javascript the quiz uses on click events to start the program and a timer is initiated for the user to finish the quiz. Each incorrect answer will take time off of the timer giving the user less time to finish the quiz. Once the quiz has been completed there is also an option to save your score and initials within the local storage. 
+
+HW Criteria: 
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
---
+++ java.js
@@ -0,0 +1,157 @@
+// countdown timer that starts as soon as begin button is clicked 
+var timeEl = document.querySelector(".time");
+
+
+var secondsLeft = 60;
+var userScore = 0;
+
+
+// created a function to initiate both timer and starting the quiz
+$(".startBtn").on("click", function(event) {
+    event.preventDefault();
+
+    //called on both function to start timer and quiz simultaneously
+    startTimer();
+    startQuiz();
+
+    //function that dictates the time allotment for the quiz
+    function startTimer(){
+        var timeInterval = setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " Seconds Remaining";
+        $(".startBtn").addClass("hide");
+
+            if(secondsLeft === 0) {
+            clearInterval(timeInterval);
+            sendMessage();
+            };
+        }, 1000);
+    };
+
+    //function that initiates the quiz, creates container for the questions and possible answers
+    function startQuiz(){
+
+        // Will need to create a container to store the questions within the HTML
+        var containerEl = document.getElementById("container")
+        // this code will ensure container is clear each time the function startQuiz is executed
+        $(".container").empty();
+
+        // Creating an element for the question text
+        var h2Tag = document.createElement ("h2");
+        // assigning text to the newly created element
+        h2Tag.textContent = questions[questionCount].question;
+        console.log(h3Tag);
+
+        // adding new elements to be used for all possible solutions of the question
+        var quesChoice = document.createElement ("ul");
+        
+
+        // loop will be used to craete a button for each possible sulotion
+        for (i=0; i<questions[questionCount].choices.length; i ++){
+            var mulChoice = document.createElement ("li");
+            var choiceBtn = document.createElement ("button")
+            choiceBtn.textContent(questions[questionCount].choices[i]);
+            choiceBtn.setAttribute("data-index", questionCount);
+            mulChoice.appendChild(choiceBtn);
+            quesChoice.appendChild(mulChoice);
+            h2Tag.prepend(mulChoice);
+            console.log(choiceBtn);
+        }
+
+
+        var questions = [
+            {
+            question: "Inside which HTML element do we put the Javascript?",
+            choices: ["<js>", "<script>", "<javascript>", "<scripting>"],
+            answer: "<script>"
+        },
+        {
+            question: "What is the correct syntax for referring to an external script called [xxx.js]?",
+            choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script src='js.xxx'>", "<script href='js.xxx'>"],
+            answer: "<script href='js.xxx'>"
+        },
+        {
+            question: "How do you call a function named 'myFunction'?",
+            choices: ["call myFunction()", "call function(myFunction())", "execute myFunction;", "myFunction()"],
+            answer: "myFunction()"
+        },
+        {
+            question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+            choices: ["if i=! 5 then", "if i <> 5", "if (i!=5)", "if (i<>5)"],
+            answer: "if (i!=5)"
+        },
+        {
+            question: "How can you add a comment in a JavaScript?",
+            choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment'", "(This is a commet)"],
+            answer: "//This is a comment"
+        }];
+
+        var questionCount = 0;
+        var correctAnswerCount = 0;
+        var incorrectAnswerCount = 0 ;
+    
+        // function to help determine user is selecting correct answer
+        document.addEventListener("click", function(e) {
+            e.preventDefault();
+            var correctAnswer;
+
+            // will need to add to userScore once answer is determined to be correct
+            if (e.target.matches("choiceBtn")){
+                var userAnswer = e.target.textContent
+                var indexData = e.target.dataset.indexData
+                userScore ++;
+            }else (correctAnswer === false) {
+                secondsLeft - 10;
+            }
+
+        });
+    };
+
+
+
+
+// // code for storing scores in local storage of the application
+//     function recordScore(event){
+
+// // local storage activity for reference
+// function init() {
+//     // Get stored todos from localStorage
+//     // Parsing the JSON string to an object
+//     var storedTodos = JSON.parse(localStorage.getItem("todos"));
+  
+//     // If todos were retrieved from localStorage, update the todos array to it
+//     if (storedTodos !== null) {
+//       todos = storedTodos;
+//     }
+  
+//     // Render todos to the DOM
+//     renderTodos();
+//   }
+  
+//   function storeTodos() {
+//     // Stringify and set "todos" key in localStorage to todos array
+//     localStorage.setItem("todos", JSON.stringify(todos));
+//   }
+  
+//   // When form is submitted...
+//   todoForm.addEventListener("submit", function(event) {
+//     event.preventDefault();
+  
+//     var todoText = todoInput.value.trim();
+  
+//     // Return from function early if submitted todoText is blank
+//     if (todoText === "") {
+//       return;
+//     }
+  
+//     // Add new todoText to todos array, clear the input
+//     todos.push(todoText);
+//     todoInput.value = "";
+  
+//     // Store updated todos in localStorage, re-render the list
+//     storeTodos();
+//     renderTodos();
+//   });
+  
+
+});
