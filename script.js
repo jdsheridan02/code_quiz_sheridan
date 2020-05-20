c08485d6d221e02b1218f38e10888bf75b339d07
jdsheridan02 <jdsheridan02@gmail.com>
2020-05-20 11:35:23 -0400

    updated script

--- script.js
+++ script.js
@@ -1,111 +1,119 @@
 // countdown timer that starts as soon as begin button is clicked 
-var timeEl = document.querySelector(".time");
-
-
-var secondsLeft = 60;
-var userScore = 0;
-
-
+var timeEl = document.querySelector(".time"),
+    secondsLeft = 60,
+    userScore = 0,
+    progress = document.getElementById("progress");
+var questions = [
+        {
+        question: "Inside which HTML element do we put the Javascript?",
+        choices: ["<js>", "<script>", "<javascript>", "<scripting>"],
+        answer: "<script>"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called [xxx.js]?",
+        choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script src='js.xxx'>", "<script href='js.xxx'>"],
+        answer: "<script href='js.xxx'>"
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        choices: ["call myFunction()", "call function(myFunction())", "execute myFunction;", "myFunction()"],
+        answer: "myFunction()"
+    },
+    {
+        question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+        choices: ["if i=! 5 then", "if i <> 5", "if (i!=5)", "if (i<>5)"],
+        answer: "if (i!=5)"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment'", "(This is a commet)"],
+        answer: "//This is a comment"
+    }];
+var questionCount = 0;
+var correctAnswerCount = 0;
+var incorrectAnswerCount = 0 ;
+    
 // created a function to initiate both timer and starting the quiz
-$(".startBtn").on("click", function(event) {
+$("#startBtn").on("click", function(event) {
     event.preventDefault();
 
     //called on both function to start timer and quiz simultaneously
     startTimer();
     startQuiz();
-
+   
+})
     //function that dictates the time allotment for the quiz
     function startTimer(){
         var timeInterval = setInterval(function(){
         secondsLeft--;
         timeEl.textContent = secondsLeft + " Seconds Remaining";
-        $(".startBtn").addClass("hide");
+        $("#startBtn").addClass(".hide");
 
             if(secondsLeft === 0) {
             clearInterval(timeInterval);
-            sendMessage();
+            timeEl.innerHTML = "⏰ Time's up!";
             };
         }, 1000);
     };
 
+
     //function that initiates the quiz, creates container for the questions and possible answers
     function startQuiz(){
+        
 
         // Will need to create a container to store the questions within the HTML
-        var containerEl = document.getElementById("container")
+        var container = document.getElementById("quizContainer")
         // this code will ensure container is clear each time the function startQuiz is executed
-        $(".container").empty();
+        $(".quizContainer").empty();
 
         // Creating an element for the question text
-        var h2Tag = document.createElement ("h2");
+        var h2Tag = document.createElement("h2");
         // assigning text to the newly created element
         h2Tag.textContent = questions[questionCount].question;
-        console.log(h3Tag);
+        console.log(h2Tag);
 
         // adding new elements to be used for all possible solutions of the question
         var quesChoice = document.createElement ("ul");
-        
-
+        console.log("****")
+    console.log(questions[questionCount].choices)
         // loop will be used to craete a button for each possible sulotion
-        for (i=0; i<questions[questionCount].choices.length; i ++){
-            var mulChoice = document.createElement ("li");
-            var choiceBtn = document.createElement ("button")
-            choiceBtn.textContent(questions[questionCount].choices[i]);
+        container.appendChild(h2Tag)
+
+        for (var i=0; i<questions[questionCount].choices.length; i ++){
+            console.log(i);
+            console.log(questions[questionCount].choices);
+            console.log(questions[questionCount].choices[i]);
+            var mulChoice = document.createElement("li");
+            var choiceBtn = document.createElement("button");
+            choiceBtn.textContent=questions[questionCount].choices[i];
             choiceBtn.setAttribute("data-index", questionCount);
             mulChoice.appendChild(choiceBtn);
-            quesChoice.appendChild(mulChoice);
-            h2Tag.prepend(mulChoice);
+            quizContainer.appendChild(mulChoice);
             console.log(choiceBtn);
         }
 
-
-        var questions = [
-            {
-            question: "Inside which HTML element do we put the Javascript?",
-            choices: ["<js>", "<script>", "<javascript>", "<scripting>"],
-            answer: "<script>"
-        },
-        {
-            question: "What is the correct syntax for referring to an external script called [xxx.js]?",
-            choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script src='js.xxx'>", "<script href='js.xxx'>"],
-            answer: "<script href='js.xxx'>"
-        },
-        {
-            question: "How do you call a function named 'myFunction'?",
-            choices: ["call myFunction()", "call function(myFunction())", "execute myFunction;", "myFunction()"],
-            answer: "myFunction()"
-        },
-        {
-            question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
-            choices: ["if i=! 5 then", "if i <> 5", "if (i!=5)", "if (i<>5)"],
-            answer: "if (i!=5)"
-        },
-        {
-            question: "How can you add a comment in a JavaScript?",
-            choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment'", "(This is a commet)"],
-            answer: "//This is a comment"
-        }];
-
-        var questionCount = 0;
-        var correctAnswerCount = 0;
-        var incorrectAnswerCount = 0 ;
-    
         // function to help determine user is selecting correct answer
         document.addEventListener("click", function(e) {
             e.preventDefault();
             var correctAnswer;
 
             // will need to add to userScore once answer is determined to be correct
-            if (e.target.matches("choiceBtn")){
+            if (e.target.matches("button")){
                 var userAnswer = e.target.textContent
                 var indexData = e.target.dataset.indexData
                 userScore ++;
-            }else (correctAnswer === false) {
-                secondsLeft - 10;
+                console.log(correctAnswer);
+                console.log(userAnswer);
+                console.log(indexData);
             }
 
+            // }else (correctAnswer === false) {
+            //     secondsLeft - 10;
+            // }
+
         });
-    };
+    }
+    
 
 
 
@@ -153,5 +161,3 @@ $(".startBtn").on("click", function(event) {
 //     renderTodos();
 //   });
   
-
-});
